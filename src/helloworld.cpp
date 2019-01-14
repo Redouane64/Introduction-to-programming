b8b5128228aaa300e27b2ef23fd458698b8cfd00
Redouane Sobaihi <s-redouane@outlook.com>
2019-01-15 00:14:28 +0700

    Add Hello World Task.

---
+++ src/helloworld.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Redouane on 15/01/2019.
+//
+
+#include <iostream>
+
+int main()
+{
+    freopen("output.txt", "w", stdout);
+
+    std::cout << "Hello, world!" << std::endl;
+
+    return 1;
+}
