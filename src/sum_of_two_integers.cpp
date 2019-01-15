1325d98f1291e2d623e4df703c89767e1f1b6b26
Redouane Sobaihi <s-redouane@outlook.com>
2019-01-15 16:33:42 +0700

    Add sum of two integers task.

---
+++ src/sum_of_two_integers.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Redouane on 15/01/2019.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    int result, a, b;
+
+    cin >> a;
+    cin >> b;
+    result = a + b;
+
+    cout << result;
+}
